11cc1a7110715bc10957bba03454034f91f79faa
jahid6292 <gulsahnerazasociety@gmail.com>
2026-02-07 15:59:15 +0530

    kabristan wise youtubeplay list embeded updated

--- src/data/kabristanConfig.jsx
+++ src/data/kabristanConfig.jsx
@@ -1,15 +1,30 @@
 const KABRISTAN_CONFIG = {
   K1: {
     name: "DPR Sahar",
-    playlistId: "PLxxxxxxx111"
+    playlistId: "PLugsKQXajiMM6aV5GdyAD2ka_g_7jhseY"
   },
   K2: {
     name: "Kabristan 2nd",
-    playlistId: "PLxxxxxxx222"
+    playlistId: "PLugsKQXajiMMKrpctMXh5csk2yPkSG4hf"
   },
   K3: {
     name: "Kabristan 3rd",
     playlistId: "PLugsKQXajiMOYNCHaN-Qsafi3zXbAk7Oy"
+
+  },
+  K4: {
+    name: "Kabristan 4th",
+    playlistId: "PLugsKQXajiMPCedwhnNCzL3fa_s0j4gzO"
+
+  },
+  K5: {
+    name: "Kabristan 5th",
+    playlistId: "PLugsKQXajiMMVdCMs0G661gjnYCS0q24c"
+
+  },
+  K6: {
+    name: "Kabristan 6th",
+    playlistId: "PLugsKQXajiMN9h-3y9IIWKD0vpPiruISu"
 
   }
 };
